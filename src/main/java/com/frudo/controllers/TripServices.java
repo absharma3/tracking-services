560552dd9c6e135fba5cdbd84c81ee058a34e98a
Abhimanyu Sharma <absharma@letsmobility.com>
2017-11-27 13:28:47 +0530

    Some changes to trip services

--- src/main/java/com/frudo/controllers/TripServices.java
+++ src/main/java/com/frudo/controllers/TripServices.java
@@ -4,9 +4,7 @@ import com.frudo.model.Trip;
 import com.frudo.repository.TripRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by abhimanyus on 11/26/17.
@@ -20,8 +18,15 @@ public class TripServices {
     TripRepository tripRepository;
 
     @RequestMapping(path = "/create" , method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public Trip createTrip(Trip tripDetails){
+    public @ResponseBody Trip createTrip(@RequestBody Trip tripDetails){
             return tripRepository.save(tripDetails);
+    }
+
+    @RequestMapping(path = "/updateDriver/{tripid}" , method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public @ResponseBody Trip updateTrip(@PathVariable("tripid") String tripId,@RequestBody Trip tripDetails){
+        Trip trip =  tripRepository.findOne(tripId);
+        trip.setDriverId(tripDetails.getDriverId());
+        return tripRepository.save(trip);
     }
 
 }
--- src/main/java/com/frudo/model/Trip.java
+++ src/main/java/com/frudo/model/Trip.java
@@ -1,12 +1,17 @@
 package com.frudo.model;
 
 import org.joda.time.DateTime;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * Created by abhimanyus on 11/26/17.
  */
 
-public abstract class  Trip {
+@Document
+public class  Trip {
+
+    @Id
     private String tripId;
     private String driverId;
     private String from;
@@ -14,4 +19,60 @@ public abstract class  Trip {
     private String vehicleId;
     private DateTime startTime;
     private DateTime endTime;
+
+    public String getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(String tripId) {
+        this.tripId = tripId;
+    }
+
+    public String getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(String driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public DateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(DateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public DateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(DateTime endTime) {
+        this.endTime = endTime;
+    }
 }
